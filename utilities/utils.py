86c8ebec3154a239912bdca90989940ec5bcdc45
EnderB0YHD <60573119+EnderB0YHD@users.noreply.github.com>
2021-06-06 19:12:46 +0200

    Delete utilities directory

--- utilities/utils.py
+++
@@ -1,7 +0,0 @@
-import discord
-def onlinecount(guild):
-    memlist = ['']
-    for member in guild.members:
-        if member.status != discord.Status.offline:
-            memlist.append(member)
-    return memlist
