0a47828053866cef911f09c9decdc5c48fb9c048
toasty marshmallow <sharmagarvit614@gmail.com>
2021-06-11 16:17:27 +0530

    rules yes, i hope i dont leak token

---
+++ utility/rules.py
@@ -0,0 +1,38 @@
+rules = [
+
+    "No NSFW - This is a simple and basic rule that everyone needs to follow. If you post any kind of NSFW it will "
+    "result in a mute. Continuing will result in a ban. (THIS INCLUDES BORDERLINE NSFW).",
+
+    "No bullying - Being a jerk to others is not allowed. Bullying will "
+    "result in a warn. Continuing will result in a kick.",
+
+    "No harassment - If you are here just to harass others because of what they are, you need to leave. We do not "
+    "accept this. Will result in a mute. Continuation will result in a ban.",
+
+    "NO DRAMA - Arguments that happens in any chat will result in a mute. Continuation will result in a kick and so "
+    "on. Just bring it to DM's please.",
+
+    "No Advertising - This includes DM advertising. The only place you are "
+    "allowed to advertise is in #deleted-channel. We only allow YouTube links here.",
+
+    "No Impersonation - Impersonating others will result in a nickname change.",
+
+    "No illegal Activity of any kind. - Any illegal activity that follows "
+    "under US Laws or other countries will result in a permanent ban and we will contact Discord support.",
+
+    "Follow the Discord Terms of Service. - Example: Being underage "
+    "will result in a ban. No raiding either.",
+
+    "HAVE COMMON SENSE. - Think before you post or ask something. "
+    "Do not post any memes about tragic events like 9/11 or animal "
+    "abuse. That will result in a mute. Continuation will result in "
+    "a Ban. This does include keeping stuff in the correct channel.",
+
+    "No ghost pinging. - Ghost pinging is pinging someone and then "
+    "deleting the ping. Will result in a warn. Spam pinging will "
+    "result in  mute.",
+
+    "No loophooling. - Loopholing is basically breaking a rule and saying "
+    "how something isn't a rule even though it is. This will result in a warn or ban."
+
+]
