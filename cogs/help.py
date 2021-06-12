01cdac8f9cdf86e8183832a933d84eb9c9f33190
The Captain <stepfightgamingyt@gmail.com>
2021-06-12 07:56:06 +0200

    advanced help moment

--- cogs/help.py
+++ cogs/help.py
@@ -21,18 +21,28 @@ class Help(commands.Cog):
         await ctx.send(embed=embed)
     """
     @commands.command()
-    async def help(self, ctx):
-        embed=discord.Embed(
-            title='help',
-            colour=discord.Colour.red()
-        )
-        cmdlist = ""
-        for command in self.client.commands:
-            if command.cog_name != 'Tags':
-              cmdlist = cmdlist + ', ' + str(command)
-        cmdlist = cmdlist.replace(', ', '\n')
-        embed.add_field(name='Command List', value=cmdlist, inline=False)
-        embed.add_field(name='And a bunch more tags', value='cant be bothered adding them here sorry', inline=False)
-        await ctx.send(embed=embed)
+    async def help(self, ctx, cmd=None):
+        if cmd == None:
+            embed=discord.Embed(
+                title='help',
+                colour=discord.Colour.red()
+            )
+            cmdlist = ""
+            for command in self.client.commands:
+                if command.cog_name != 'Tags':
+                    cmdlist = cmdlist + ', ' + str(command)
+            cmdlist = cmdlist.replace(', ', '\n')
+            embed.add_field(name='Command List', value=cmdlist, inline=False)
+            embed.add_field(name='And a bunch more tags', value='cant be bothered adding them here sorry', inline=False)
+            await ctx.send(embed=embed)
+        else:
+            for command in self.client.commands:
+                if cmd.lower() == str(command).lower():
+                    embed = discord.Embed(
+                        title=command,
+                        description=command.help,
+                        colour=discord.Colour.red()
+                    )
+                    await ctx.send(embed=embed)
 def setup(client):
     client.add_cog(Help(client))
