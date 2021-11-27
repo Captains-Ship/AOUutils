3d4b32ef2cc890411ab850b99605b0d05f4918a1
The Captain <81252038+Captain8771@users.noreply.github.com>
2021-11-27 10:43:13 +0100

    gfjndfj

--- cogs/misc.py
+++ cogs/misc.py
@@ -14,10 +14,10 @@ class Misc(commands.Cog):
     @commands.command(description="Chatbot")
     @commands.cooldown(1, 3, type=discord.ext.commands.BucketType.user)
     async def chat(self, ctx, *, text):
-        await self.client.refreshHttp()
+        cs = await self.client.refreshHttp()
         text = quote(text, safe="")
         key = getconfig()["tokens"]["nuggies"]
-        async with self.client.cs.get(f"https://api.nuggetdev.com/chat?message={text}&key={key}") as resp:
+        async with cs.get(f"https://api.nuggetdev.com/chat?message={text}&key={key}") as resp:
             if resp.status == 200:
                 await ctx.send(resp.json())
 
