5c8008faa80d851fbfca3a628f6ad3cb97632668
The Captain <81252038+Captain8771@users.noreply.github.com>
2021-11-27 10:44:53 +0100

    is this a thing

--- cogs/misc.py
+++ cogs/misc.py
@@ -17,6 +17,7 @@ class Misc(commands.Cog):
         cs = await self.client.refreshHttp()
         text = quote(text, safe="")
         key = getconfig()["tokens"]["nuggies"]
+        await ctx.send(cs.closed)
         async with cs.get(f"https://api.nuggetdev.com/chat?message={text}&key={key}") as resp:
             if resp.status == 200:
                 await ctx.send(resp.json())
