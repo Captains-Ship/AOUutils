7866fa4b21d0a9849e5837ad7cee40ae503b2056
Captain8771 <81252038+Captain8771@users.noreply.github.com>
2024-02-24 12:41:09 +0100

    some logger stuff

--- logger.py
+++ logger.py
@@ -21,38 +21,18 @@ class logger:
     def debug(text: str):
         print(f'{crayons.cyan(f"[AOUutils/Debug] {text}")}')
 
-    @staticmethod
-    def LG(text: str):
-        print(f'{crayons.cyan(f"[AOUutils/Log] {text}")}')
-
-    @staticmethod
-    def I(text: str):
-        print(f'{crayons.cyan(f"[AOUutils/Info] {text}")}')
-
-    @staticmethod
-    def ERR(text: str):
-        print(f'{crayons.cyan(f"[AOUutils/Error] {text}")}')
-
-    @staticmethod
-    def DBG(text: str):
-        print(f'{crayons.cyan(f"[AOUutils/Debug] {text}")}')
-
 
 def log(text: str):
-    print(f'[AOUutils/Log] {text}')
+    logger.log(text)
 
 
 def error(text: str):
-    print(f'{crayons.red(f"[AOUutils/Error] {text}")}')
+    logger.error(text)
 
 
 def info(text: str):
-    print(f'{crayons.green(f"[AOUutils/info] {text}")}')
+    logger.info(text)
 
 
 def debug(text: str):
-    print(f'{crayons.cyan(f"[AOUutils/Debug] {text}")}')
-
-
-def warn(text: str):
-    print(f'{crayons.yellow(f"[AOUutils/Warn] {text}")}')
+    logger.debug(text)
